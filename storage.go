f99ac747783196aaea1e0c4f706964d910db9a41
Ojas Sethi <sethi.ojas@gmail.com>
2021-07-11 12:47:25 +0530

    Added set method to dataStore

--- storage.go
+++ storage.go
@@ -3,3 +3,9 @@ package chord
 // dataStore is an alias to map data structure
 // with string type keys and string type values
 type dataStore map[string]string
+
+// Save a key-value pair
+func (data dataStore) set(key, value string) {
+	data[key] = value
+}
+
