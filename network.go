d7f9592d27ace095464d0df3737db569892a50b3
Ojas Sethi <sethi.ojas@gmail.com>
2021-07-11 13:37:54 +0530

    Initial commit

---
+++ network.go
@@ -0,0 +1,38 @@
+package chord
+
+import (
+	"crypto/sha1"
+	"net"
+	"net/http"
+	"net/rpc"
+)
+
+func createNewNode(address string) (*Node, error) {
+	h := sha1.New()
+	h.Write([]byte(address))
+
+	node := &Node{
+		id:          h.Sum(nil),
+		address:     address,
+		predecessor: nil,
+		fingerTable: nil,
+	}
+
+	rpc.Register(node)
+	rpc.HandleHTTP()
+	l, err := net.Listen("tcp", address)
+	if err != nil {
+		return nil, LISTEN_ERROR
+	}
+
+	go http.Serve(l, nil)
+
+	client, err := rpc.DialHTTP("tpc", address)
+	if err != nil {
+		return nil, DIALING_ERROR
+	}
+
+	node.self = client
+
+	return node, nil
+}
