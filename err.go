744f6e74ea7280c31627ba5ab9ba1602de48cd2f
Ojas Sethi <sethi.ojas@gmail.com>
2021-07-12 15:33:41 +0530

    Added new error: ErrFailedToReach

--- err.go
+++ err.go
@@ -5,4 +5,5 @@ import "errors"
 var (
 	ErrUnableToListen = errors.New("error: rpc server unable to listen on specified addr:port")
 	ErrUnableToDial   = errors.New("error: unable to dial the address of rpc server")
+	ErrFailedToReach  = errors.New("error: unable to reach rpc server")
 )
