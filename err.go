3ec45dfa0d4fcf5d5f2a810ac2a0ca2564b64388
Ojas Sethi <sethi.ojas@gmail.com>
2021-07-14 11:15:09 +0530

    Added ErrNodeAlreadyExists

--- err.go
+++ err.go
@@ -3,7 +3,8 @@ package chord
 import "errors"
 
 var (
-	ErrUnableToListen = errors.New("error: rpc server unable to listen on specified addr:port")
-	ErrUnableToDial   = errors.New("error: unable to dial the address of rpc server")
-	ErrFailedToReach  = errors.New("error: unable to reach rpc server")
+	ErrUnableToListen    = errors.New("error: rpc server unable to listen on specified addr:port")
+	ErrUnableToDial      = errors.New("error: unable to dial the address of rpc server")
+	ErrFailedToReach     = errors.New("error: unable to reach rpc server")
+	ErrNodeAlreadyExists = errors.New("error: node with same id already exists")
 )
