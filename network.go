e9fa8c19f1e2b239654ee99a3c9ba28595585308
Ojas Sethi <sethi.ojas@gmail.com>
2021-07-14 13:26:47 +0530

    added comments to createNewNode

--- network.go
+++ network.go
@@ -8,9 +8,11 @@ import (
 )
 
 func createNewNode(address string, joinNodeAddr string) (*Node, error) {
+	// create []byte from address using sha1
 	h := sha1.New()
 	h.Write([]byte(address))
 
+	// Initialize node
 	node := &Node{
 		id:             h.Sum(nil),
 		address:        address,
@@ -18,33 +20,42 @@ func createNewNode(address string, joinNodeAddr string) (*Node, error) {
 		predecessorRPC: nil,
 	}
 
+	// start rpc server for node
 	rpc.Register(node)
 	rpc.HandleHTTP()
 	l, err := net.Listen("tcp", address)
 	if err != nil {
 		return nil, ErrUnableToListen
 	}
-
 	go http.Serve(l, nil)
 
+	// create rpc client for node
 	client, err := rpc.DialHTTP("tpc", address)
 	if err != nil {
 		return nil, ErrUnableToDial
 	}
-
 	node.self = client
 
+	// populate finger table
+	// successor of node is node itself if there
+	// aren't any other nodes in the network
 	node.fingerTable = append(node.fingerTable, &Finger{node.id, node.self})
 
+	// empty join address means new network
+	// hence return the new node
 	if joinNodeAddr == "" {
 		return node, nil
 	}
+
+	// join address was not empty means
+	// this node has to join exitsting network
 
 	joinNodeClient, err := rpc.DialHTTP("tcp", joinNodeAddr)
 	if err != nil {
 		return nil, ErrUnableToDial
 	}
 
+	// find appropriate successor of new node
 	var successor rpc.Client
 	joinNodeClient.Call("Node.Successor", node.id, &successor)
 
@@ -55,9 +66,12 @@ func createNewNode(address string, joinNodeAddr string) (*Node, error) {
 		return nil, ErrNodeAlreadyExists
 	}
 
+	// update first finger to point to successor
 	node.fingerTable[0].id = successorId
 	node.fingerTable[0].node = &successor
 
+	// notify successor that new node might
+	// be its predecessor
 	successor.Call("Node.Notify", &node, "")
 
 	return node, nil
