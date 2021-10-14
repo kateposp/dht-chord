1be5aef607a9e7bec6867653a5292c10dec8f17d
Ojas Sethi <sethi.ojas@gmail.com>
2021-10-14 10:04:20 +0530

    removed saving of data into chord network.

--- example/node2.go
+++ example/node2.go
@@ -4,8 +4,6 @@ import (
 	"fmt"
 	"os"
 	"os/signal"
-	"strconv"
-	"time"
 
 	chord "github.com/sethiojas/dht-chord"
 )
@@ -17,28 +15,28 @@ func main() {
 		return
 	}
 
-	ch := make(chan struct{})
-	go func() {
-		count := 500
-		for {
-			ticker := time.NewTicker(5 * time.Second)
-			select {
-			case <-ticker.C:
-				count++
-				key := strconv.Itoa(count)
-				value := "hello-" + strconv.Itoa(count)
-				node.Save(key, value)
-			case <-ch:
-				ticker.Stop()
-				return
-			}
-		}
-	}()
+	// ch := make(chan struct{})
+	// go func() {
+	// 	count := 500
+	// 	for {
+	// 		ticker := time.NewTicker(5 * time.Second)
+	// 		select {
+	// 		case <-ticker.C:
+	// 			count++
+	// 			key := strconv.Itoa(count)
+	// 			value := "hello-" + strconv.Itoa(count)
+	// 			node.Save(key, value)
+	// 		case <-ch:
+	// 			ticker.Stop()
+	// 			return
+	// 		}
+	// 	}
+	// }()
 
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt)
 	<-c
-	close(ch)
+	// close(ch)
 	close(c)
 	node.Stop()
 }
